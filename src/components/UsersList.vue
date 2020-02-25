6163ac0a6ae15aca566335e58699167a31b56aac
Brandon Johnson <bmj1985@gmail.com>
2020-02-25 15:34:04 -0700

    WIP Admin Dashboard

---
+++ src/components/UsersList.vue
@@ -0,0 +1,106 @@
+<template>
+  <FeathersVuexFind service="users" :query="{}" watch="query" v-if="isAdmin">
+    <section slot-scope="{ items: users }">
+      <b-table
+        :data="users"
+        :mobile-cards="hasMobileCards"
+        v-if="!loading"
+        paginated
+        backend-pagination
+        :total="total"
+        :per-page="perPage"
+        sticky-header
+        :default-sort-direction="defaultSortDirection"
+        default-sort="isApprovedByAdmin"
+        :sortIcon="sortIcon"
+      >
+        <template slot-scope="props">
+          <b-table-column field="id" label="ID" width="40" numeric>
+            {{ props.row.id }}
+          </b-table-column>
+
+          <b-table-column field="name" label="Name" searchable>
+            {{ props.row.name }}
+          </b-table-column>
+          <b-table-column field="picture" label="Picture">
+            <div class="profile-picture-container">
+              <img class="is32x32" :src="props.row.profilePicture" />
+            </div>
+          </b-table-column>
+
+          <b-table-column label="Approved" centered>
+            <span
+              :class="[
+                props.row.isApprovedByAdmin ? 'is-success' : 'is-danger',
+                'tag'
+              ]"
+            >
+              {{ props.row.isApprovedByAdmin }}
+            </span>
+          </b-table-column>
+          <b-table-column field="permissions" label="Roles" centered>
+            <div v-for="(role, i) in props.row.permissions" :key="i">
+              {{ role }}
+            </div>
+          </b-table-column>
+          <b-table-column field="approvedBy" label="Approved By">
+            {{
+              props.row.adminApprovalData &&
+                props.row.adminApprovalData.adminName
+            }}
+          </b-table-column>
+          <!-- <b-table-column field="approvedOn" label="Approved On">
+            {{
+              props.row.adminApprovalData &&
+                format(props.row.adminApprovalData.date)
+            }}
+          </b-table-column> -->
+        </template>
+      </b-table>
+    </section>
+  </FeathersVuexFind>
+</template>
+
+<script>
+import Vue from 'vue'
+import { mapGetters, mapState } from 'vuex'
+import { format } from 'date-fns'
+export default Vue.extend({
+  name: 'UsersList',
+  data() {
+    return {
+      hasMobileCards: true,
+      total: 100,
+      loading: false,
+      sortField: 'approvedBy',
+      sortOrder: 'desc',
+      defaultSortOrder: 'desc',
+      page: 10,
+      perPage: 10,
+      isPaginated: true,
+      isPaginationSimple: false,
+      paginationPosition: 'bottom',
+      defaultSortDirection: 'asc',
+      sortIcon: 'arrow-up',
+      sortIconSize: 'is-small',
+      currentPage: 1
+    }
+  },
+  computed: {
+    ...mapState('auth', { loading: 'isAuthenticatePending' }),
+    ...mapGetters(['isAdmin'])
+  },
+  methods: {
+    format(date) {
+      return format(date, 'MMM DD, YYYY')
+    }
+  }
+})
+</script>
+
+<style>
+.profile-picture-container {
+  height: 32px;
+  width: 32px;
+}
+</style>
--- src/views/UserProfile.vue
+++ src/views/UserProfile.vue
@@ -1,47 +1,74 @@
 <template>
-  <Container>
-    <p>Email: {{ email }}</p>
-    <p>Name: {{ name }}</p>
-    <p>Profile Picture: {{ profilePicture }}</p>
+  <Container class="outer-container">
+    <div class="container">
+      <p>Email: {{ email }}</p>
+      <p>Name: {{ name }}</p>
+      <div class="profile-container">
+        Profile Picture:
+        <div class="image-container">
+          <img class="is128x128" :src="profilePicture" />
+        </div>
+      </div>
+    </div>
+    <UsersList class="users-list" />
   </Container>
 </template>
 
 <script>
 import Vue from 'vue'
 import Container from '../UIComponents/Container'
+import UsersList from '../components/UsersList'
+import { mapGetters, mapState } from 'vuex'
 export default Vue.extend({
   name: 'UserProfile',
-  components: { Container },
+  components: { Container, UsersList },
   computed: {
+    ...mapState('auth', { loading: 'isAuthenticatePending' }),
+    ...mapGetters('auth', { user: 'user' }),
     email() {
-      return (
-        this.$store &&
-        this.$store.state &&
-        this.$store.state.auth &&
-        this.$store.state.auth.user &&
-        this.$store.state.auth.user.email
-      )
+      if (!this.loading) {
+        return this.user.email
+      }
+      return ''
     },
     name() {
-      return (
-        this.$store &&
-        this.$store.state &&
-        this.$store.state.auth &&
-        this.$store.state.auth.user &&
-        this.$store.state.auth.user.name
-      )
+      if (!this.loading) {
+        return this.user.name
+      }
+      return ''
     },
     profilePicture() {
-      return (
-        this.$store &&
-        this.$store.state &&
-        this.$store.state.auth &&
-        this.$store.state.auth.user &&
-        this.$store.state.auth.user.profilePicture
-      )
+      if (!this.loading) {
+        return this.user.profilePicture
+      }
+      return ''
     }
   }
 })
 </script>
 
-<style></style>
+<style scoped>
+.outer-container {
+  align-items: flex-start;
+  justify-content: flex-start;
+}
+.container {
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+  max-width: 300px;
+  height: 400px;
+  margin: 0;
+}
+.image-container {
+  width: 128px;
+  height: 128px;
+}
+.profile-container {
+  flex-direction: row;
+}
+.users-list {
+  width: 100%;
+}
+</style>
