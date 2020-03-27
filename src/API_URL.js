f3379f1b94226569a0830907ce19a253910e1bba
Brandon Johnson <bmj1985@gmail.com>
2020-03-27 15:18:38 -0600

    update API_URL

--- src/API_URL.js
+++ src/API_URL.js
@@ -4,6 +4,6 @@ function getApiUrl() {
   if (window.location.hostname.indexOf('staging') !== -1) {
     return 'https://gmrapi-staging.herokuapp.com'
   } else if (window.location.hostname.indexOf('goldenmountainrunners') !== -1) {
-    return 'https://gmrapi-staging.herokuapp.com'
+    return 'https://gmrapi.herokuapp.com'
   } else return 'http://localhost:3030'
 }
---
+++ src/store/services/announcements.js
@@ -0,0 +1,58 @@
+import feathersClient, {
+  makeServicePlugin,
+  BaseModel
+} from '../feathers-client'
+
+class Announcement extends BaseModel {
+  // constructor(data, options) {
+  //   super(data, options)
+  // }
+  // Required for $FeathersVuex plugin to work after production transpile.
+  static modelName = 'Announcement'
+  // Define default properties here
+  static instanceDefaults() {
+    return {
+      content: '',
+      permissions: ['public']
+    }
+  }
+}
+const servicePath = 'announcements'
+const servicePlugin = makeServicePlugin({
+  Model: Announcement,
+  service: feathersClient.service(servicePath),
+  servicePath
+})
+
+// Setup the client-side Feathers hooks.
+feathersClient.service(servicePath).hooks({
+  before: {
+    all: [],
+    find: [],
+    get: [],
+    create: [],
+    update: [],
+    patch: [],
+    remove: []
+  },
+  after: {
+    all: [],
+    find: [],
+    get: [],
+    create: [],
+    update: [],
+    patch: [],
+    remove: []
+  },
+  error: {
+    all: [],
+    find: [],
+    get: [],
+    create: [],
+    update: [],
+    patch: [],
+    remove: []
+  }
+})
+
+export default servicePlugin
